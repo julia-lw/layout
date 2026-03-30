5f70a3293fb6b9cbd5e8e76bb657d921ac44edcb
Júlia L. W. <julia_wecolovis@estudante.sesisenai.org.br>
2026-03-30 14:40:20 -0300

    Update app.routes.ts

--- layout/src/app/app.routes.ts
+++ layout/src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { Farmador } from './pages/farmador/farmador';
+import { Moggador } from './pages/moggador/moggador';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    {path: }
+    {path: 'farmador', component: Farmador},
+    {path: 'moggador', component: Moggador},
+    {path: '', component: Home},
 ];
